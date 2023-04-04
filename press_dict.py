7042a3a74cc65afe11911173f3e91d9e8600f3dc
Yoon <74040890+memoday@users.noreply.github.com>
2023-04-04 17:12:15 +0900

    Create press_dict.py
    - 통합 dictionary 만드는 중

---
+++ press_dict.py
@@ -0,0 +1,116 @@
+pressData = {
+    'www.sisa-news.com': {
+        'name': '시사뉴스',
+        'datetimeSelector': '#container > div.column.col73.mb00 > div:nth-child(1) > div > div.arv_005_01 > div.fix_art_top > div > div > ul.art_info > li:nth-child(2)',
+        'datetimeFormat': '%Y.%m.%d %H:%M:%S',
+        'datetimeTrim': 3
+    },
+    'www.ilyosisa.co.kr': {
+        'name': '일요시사'
+    },
+    'www.skyedaily.com': {
+        'name': '스카이데일리'
+    },
+    'idsn.co.kr': {
+        'name': '매일안전신문'
+    },
+    'www.siminilbo.co.kr': {
+        'name': '시민일보'
+    },
+    'www.wsobi.com': {
+        'name': '여성소비자신문'
+    },
+    'realty.chosun.com': {
+        'name': '땅집고'
+    },
+    'www.the-pr.co.kr': {
+        'name': 'The PR Time'
+    },
+    'www.vegannews.co.kr': {
+        'name': '비건뉴스'
+    },
+    'www.wikitree.co.kr': {
+        'name': '위키트리'
+    },
+    'www.viva100.com': {
+        'name': '브릿지경제'
+    },
+    'www.discoverynews.kr': {
+        'name': '디스커버리뉴스'
+    },
+    'www.joongboo.com': {
+        'name': '중부일보'
+    },
+    'www.nspna.com': {
+        'name': 'NSP통신'
+    },
+    'www.asiatoday.co.kr': {
+        'name': '아시아투데이'
+    },
+    'www.kihoilbo.co.kr': {
+        'name': '기호일보'
+    },
+    'www.thedailypost.kr': {
+        'name': '데일리포스트'
+    },
+    'www.donga.com': {
+        'name': '동아일보'
+    },
+    'ch1.skbroadband.com': {
+        'name': 'SK브로드밴드',
+        'datetimeSelector': 'body > div.wrapper > div.container > div.contentBox > div > div.wrap_content_view > div.content_metadata > dl > dd > div > span',
+        'datetimeFormat': '%Y-%m-%d %H:%M:%S'
+    },
+    'www.obsnews.co.kr': {
+        'name': 'OBS'
+    },
+    'www.harpersbazaar.co.kr': {
+        'name': '하퍼스바자'
+    },
+    'mbnmoney.mbn.co.kr': {
+        'name': '매일경제TV'
+    },
+    'www.jeonmae.co.kr': {
+        'name': '전국매일신문'
+    },
+    'www.queen.co.kr': {
+        'name': 'Queen'
+    },
+    'www.foodneconomy.com': {
+        'name': '푸드경제신문'
+    },
+    'www.ktv.go.kr': {
+        'name': 'KTV국민방송'
+    },
+    'www.areyou.co.kr': {
+        'name': '아유경제'
+    },
+    'www.newsmaker.or.kr': {
+        'name': '뉴스메이커'
+    },
+    'thepublic.kr': {
+        'name': '더퍼블릭'
+    },
+    'www.ekn.kr': {
+        'name': '에너지경제'
+    },
+    'www.goodkyung.com': {
+        'name': '굿모닝경제'
+    },
+    'www.newstomato.com': {
+        'name': '뉴스토마토'
+    },
+    'www.sisaon.co.kr': {
+        'name': '시사오늘'
+    },
+    'hobbyen-news.com': {
+        'name': '하비엔뉴스'
+    },
+    'www.job-post.co.kr': {
+        'name': '잡포스트'
+    },
+    'www.elle.co.kr': {
+        'name': '엘르'
+    },
+}
+
