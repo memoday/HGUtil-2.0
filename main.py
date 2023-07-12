796d429425f09a37d0efbab9545c93747ad8abba
Yoon <74040890+memoday@users.noreply.github.com>
2023-07-12 15:12:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import toMessage
 import webbrowser
 from PyQt5.QtCore import Qt
 
-__version__ = 'v1.3.1'
+__version__ = 'v1.3.2'
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
